47fe2fdb200fde8c996b9e6e05d28c9550f46345
Joel Egerton <egertonjoel@gmail.com>
2025-04-04 20:14:54 +0100

    more more more

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,23 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_path, notice: "Logged out"
   end
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in Successfully"
+    else
+      flash[:alert] = "Invalid email or password"
+      render :new, status: :unprocessable_entity
+    end
+
+
+
+  end
+
 end
