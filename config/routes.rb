fcff2c52703d2d1b30e4f78ff68d7cd979b48534
Joel Egerton <egertonjoel@gmail.com>
2025-04-04 20:02:20 +0100

    And some More

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
